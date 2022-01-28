b501efb19f448da6f3b465025782345e4c260ed0
Ripslash <92413947+Ripslash@users.noreply.github.com>
2022-01-28 12:03:43 -0800

    added some commented stuff

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -1,32 +1,64 @@
+// // File reading code from https://howtodoinjava.com/java/io/java-read-file-to-string-examples/
+// import java.io.IOException;
+// import java.nio.file.Files;
+// import java.nio.file.Path;
+// import java.util.ArrayList;
+
+// //javac -cp ".;lib\junit-4.13.2.jar;lib\hamcrest-core-1.3.jar" MarkdownParseTest.java
+// //java -cp ".;lib/junit-4.13.2.jar;lib/hamcrest-core-1.3.jar" org.junit.runner.JUnitCore MarkdownParseTest
+
+// public class MarkdownParse {
+//     public static ArrayList<String> getLinks(String markdown) {
+//         ArrayList<String> toReturn = new ArrayList<>();
+//         // find the next [, then find the ], then find the (, then take up to
+//         // the next )
+//         boolean shatter = true;
+//         int currentIndex = 0;
+//         while(currentIndex < markdown.length()) {
+//             int nextOpenBracket = markdown.indexOf("[", currentIndex);
+//             if(nextOpenBracket == -1) break;
+//             int nextCloseBracket = markdown.indexOf("]", nextOpenBracket);
+//             if(nextCloseBracket == -1) break;
+//             int openParen = markdown.indexOf("(", nextCloseBracket);
+//             if(openParen == -1) break;
+//             int closeParen = markdown.indexOf(")", openParen);
+//             if(closeParen == -1) break;
+//             //while(shatter == true) {}
+//             toReturn.add(markdown.substring(openParen + 1, closeParen));
+//             currentIndex = closeParen + 1;
+//             System.out.println(currentIndex);
+//         }
+//         return toReturn;
+//     }
+//     public static void main(String[] args) throws IOException {
+// 		Path fileName = Path.of(args[0]);
+// 	    String contents = Files.readString(fileName);
+//         ArrayList<String> links = getLinks(contents);
+//         System.out.println(links);
+//     }
+// }
+
+
+
 // File reading code from https://howtodoinjava.com/java/io/java-read-file-to-string-examples/
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
 
-//javac -cp ".;lib\junit-4.13.2.jar;lib\hamcrest-core-1.3.jar" MarkdownParseTest.java
-//java -cp ".;lib/junit-4.13.2.jar;lib/hamcrest-core-1.3.jar" org.junit.runner.JUnitCore MarkdownParseTest
-
 public class MarkdownParse {
     public static ArrayList<String> getLinks(String markdown) {
         ArrayList<String> toReturn = new ArrayList<>();
         // find the next [, then find the ], then find the (, then take up to
         // the next )
-        boolean shatter = true;
         int currentIndex = 0;
         while(currentIndex < markdown.length()) {
             int nextOpenBracket = markdown.indexOf("[", currentIndex);
-            if(nextOpenBracket == -1) break;
             int nextCloseBracket = markdown.indexOf("]", nextOpenBracket);
-            if(nextCloseBracket == -1) break;
             int openParen = markdown.indexOf("(", nextCloseBracket);
-            if(openParen == -1) break;
             int closeParen = markdown.indexOf(")", openParen);
-            if(closeParen == -1) break;
-            //while(shatter == true) {}
             toReturn.add(markdown.substring(openParen + 1, closeParen));
             currentIndex = closeParen + 1;
-            System.out.println(currentIndex);
         }
         return toReturn;
     }
@@ -36,4 +68,4 @@ public class MarkdownParse {
         ArrayList<String> links = getLinks(contents);
         System.out.println(links);
     }
-}
+} 
