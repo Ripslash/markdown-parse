4e98f0355d518b34679a29615e402498a5f1e208
Ripslash <92413947+Ripslash@users.noreply.github.com>
2022-02-24 17:50:29 -0800

    adding tests

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -81,4 +81,25 @@ public class MarkdownParseTest {
         List morenames = List.of("https://something.com");
         assertEquals(morenames, othername);
     }
+    @Test
+    public void test9() throws IOException {
+        String variablename = Files.readString(Path.of("test-file(9).md"));
+        ArrayList<String> othername = MarkdownParse.getLinks(variablename);
+        List morenames = List.of("`google.com, google.com, ucsd.edu");
+        assertEquals(morenames, othername);
+    }
+    @Test
+    public void test10() throws IOException {
+        String variablename = Files.readString(Path.of("test-file(10).md"));
+        ArrayList<String> othername = MarkdownParse.getLinks(variablename);
+        List morenames = List.of("a.com, example.com");
+        assertEquals(morenames, othername);
+    }
+    @Test
+    public void test11() throws IOException {
+        String variablename = Files.readString(Path.of("test-file(11).md"));
+        ArrayList<String> othername = MarkdownParse.getLinks(variablename);
+        List morenames = List.of("https://ucsd-cse15l-w22.github.io/, ");
+        assertEquals(morenames, othername);
+    }
 }
